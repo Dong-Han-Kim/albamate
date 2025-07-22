ece3d0ce6d394c5a6f934c59860bcb909649b249
Dong-Han-Kim <dh32110@gmail.com>
2025-07-22 13:13:08 +0900

    🐛 Fix: searchParams 타입 수정

--- src/app/(owner)/addform/page.tsx
+++ src/app/(owner)/addform/page.tsx
@@ -5,19 +5,15 @@ import AddformClient from '@/features/owner/addform/components/AddformClient';
 export const generateMetadata = async ({
   searchParams,
 }: {
-  searchParams?: Record<string, string | string[]>;
+  searchParams: Promise<{ formId?: string }>;
 }): Promise<Metadata> => {
-  const formId = Array.isArray(searchParams?.formId)
-    ? searchParams.formId[0]
-    : searchParams?.formId;
-
+  const formId = (await searchParams).formId;
   if (formId) {
     return {
       title: `알바폼 수정하기`,
       description: `알바폼을 수정하는 페이지입니다.`,
     };
   }
-
   return {
     title: '새 알바폼 만들기',
     description: '새로운 알바폼을 작성하는 페이지입니다.',
@@ -27,11 +23,9 @@ export const generateMetadata = async ({
 const AddformPage = async ({
   searchParams,
 }: {
-  searchParams?: Record<string, string | string[]>;
+  searchParams: Promise<{ formId?: string }>;
 }) => {
-  const formId = Array.isArray(searchParams?.formId)
-    ? searchParams.formId[0]
-    : searchParams?.formId;
+  const formId = (await searchParams).formId;
   return <AddformClient formId={formId} />;
 };
 export default AddformPage;
